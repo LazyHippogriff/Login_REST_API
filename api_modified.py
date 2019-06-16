4b8d9cd362ed5f50c43c4d6edd9906f260357677
Vishal Sharma <37897339+vishal14101993@users.noreply.github.com>
2019-06-16 11:52:44 +0530

    Update api_modified.py

--- api_modified.py
+++ api_modified.py
@@ -17,29 +17,31 @@ class HelloWorld(Resource):
 
 class getUserDetailsAPI(Resource):
 
-    def get(self):
+    def get(self,userID):
       f = open("/home/ec2-user/python_codes/output.txt","w")
       try:
-        some_json = request.get_json()
-        uId = some_json['ID']
+    #    some_json = request.get_json()
+    #    uId = some_json['ID']
+        uId = userID
         if uId is None:
           f.close()
-          return({'error':'Missing User ID in the Json.'},409)
+          return({'error':'Missing User ID in the Json.','success':''},409)
 
         f.write("UID: "+str(uId))
         h_key = uId + "_profile"
         userDetails = r.get(h_key)
         if userDetails is None:
           f.close()
-          return({'error':'User ID not found.'},404)
+          return({'error':'User ID not found.','success':''},404)
 
         f.write("User Details: "+ userDetails.decode('utf-8'))
         f.close()
-        return ({'user_id': uId, 'user_details': userDetails.decode('utf-8')}, 200)
+        result_json = { 'user_id': uId, 'user_details': userDetails.decode('utf-8')}
+        return ({'error':'','success':result_json}, 200)
 
       except:
         f.close()
-        return ({'error': 'exception'}, 400)
+        return ({'error': 'exception','success':''}, 400)
 
 
 class putUserDetailsAPI(Resource):
@@ -51,7 +53,7 @@ class putUserDetailsAPI(Resource):
         uId = some_json['ID']
         if uId is None:
           f.close()
-          return({'error':'Missing User ID in the Json.'},409)
+          return({'error':'Missing User ID in the Json.','success':''},409)
 
         f.write("UID: "+str(uId))
         del some_json['ID']
@@ -59,11 +61,11 @@ class putUserDetailsAPI(Resource):
         h_key = uId + "_profile"
         r.set(h_key, str(some_json))
         f.close()
-        return ({'result': 'OK'}, 200)
+        return ({'error':'','success': 'OK'}, 200)
 
       except:
         f.close()
-        return ({'error': 'exception'}, 400)
+        return ({'error': 'exception','success':''}, 400)
 
 
 class verifyOtpAPI(Resource):
@@ -79,21 +81,21 @@ class verifyOtpAPI(Resource):
         l_otp = r.get(l_key)
         if l_otp is None:
             f.close()
-            return({'error':'OTP expired. Get new OTP.'},400)
+            return({'error':'OTP expired. Get new OTP.','success':''},400)
         else:
           l_otp = l_otp.decode('utf-8')
         f.write("l_otp: " + str(l_otp) + "\nl_otpFromUser: \
 " + str(l_otpFromUser))
         if l_otp != l_otpFromUser:
             f.close()
-            return({'error':'Wrong OTP.'},400)
+            return({'error':'Wrong OTP.','success':''},400)
         else:
             f.close()
             return ({'user_id': user}, 200)
 
       except:
         f.close()
-        return ({'error': 'exception'}, 400)
+        return ({'error': 'exception','success':''}, 400)
 
 class loginAPI(Resource):
 
@@ -102,21 +104,21 @@ class loginAPI(Resource):
         some_json = request.get_json()
         user = some_json['country_code'] + "_" + some_json['phone_number']
         mob_no = some_json['country_code'] + some_json['phone_number']
-        otpobj =  sendotp.sendotp('9Af2uB93ad8','Your OTP is {{otp}}.')
+        otpobj =  sendotp.sendotp('93539Af2uB9Za7o56063ad8','Your OTP is {{otp}}.')
         l_otp = random.randint(1000,9999)
         l_key = user + "_OTP"
         r.setex(l_key,150,l_otp)
         otpobj.send(mob_no,'msgind',l_otp)
-        return ({'result': 'OK'}, 200)
+        return ({'error':'','success': 'OK'}, 200)
       except:
-        return ({'error': 'exception'}, 400)
+        return ({'error': 'exception','success':''}, 400)
 
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(verifyOtpAPI, '/verifyOTP')
 api.add_resource(loginAPI, '/login')
 api.add_resource(putUserDetailsAPI, '/putUserDetails')
-api.add_resource(getUserDetailsAPI, '/getUserDetails')
+api.add_resource(getUserDetailsAPI, '/getUserDetails/<userID>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5010)
